7ce360dbcb8747b3ce7291ac8247bab90e7a8ab2
JoaoVitor <vitorsantanalins@gmail.com>
2025-07-02 20:24:27 -0300

    Criação do UsuarioRepository

---
+++ src/main/java/com/projetinhosgit/rh/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.projetinhosgit.rh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projetinhosgit.rh.model.Usuario;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+    
+Optional<Usuario> findByLogin(String login);
+    
+}
